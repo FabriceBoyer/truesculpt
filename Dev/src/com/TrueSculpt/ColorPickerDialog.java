bd18637aba121ab12a320c5f5cafb35bd9945935
fabrice boyer <fabrice.boyer@gmail.com>
2010-09-28 00:22:31 +0200

    AutoSave

--- Dev/src/com/TrueSculpt/ColorPickerDialog.java
+++ Dev/src/com/TrueSculpt/ColorPickerDialog.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.graphics;
+package com.TrueSculpt;
 
 import android.os.Bundle;
 import android.app.Dialog;
--- Dev/src/com/TrueSculpt/GLSurfaceViewActivity.java
+++ Dev/src/com/TrueSculpt/GLSurfaceViewActivity.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.apis.graphics;
+package com.TrueSculpt;
 
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
