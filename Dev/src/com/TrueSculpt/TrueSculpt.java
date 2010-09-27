1fd9c1bbc0bcc7bade7186cb0f92e299e553dc4e
fabrice boyer <fabrice.boyer@gmail.com>
2010-09-28 00:37:32 +0200

    AutoSave

--- Dev/src/com/TrueSculpt/TrueSculpt.java
+++ Dev/src/com/TrueSculpt/TrueSculpt.java
@@ -1,13 +1,22 @@
 package com.TrueSculpt;
 
+import com.TrueSculpt.ColorPickerDialog;
+import com.TrueSculpt.ColorPickerDialog.OnColorChangedListener;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TrueSculpt extends Activity {
+public class TrueSculpt extends Activity implements OnColorChangedListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        new ColorPickerDialog(this, this, 0).show();
+    }
+    
+    public void colorChanged(int color) {
+      //  mPaint.setColor(color);
     }
 }
