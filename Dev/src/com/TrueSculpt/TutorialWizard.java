0ac3c489091e1491733da05378456b160100c091
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-01 22:24:22 +0100

    NEW - task 3: Startup Tutorial Wizard http://code.google.com/p/truesculpt/issues/detail?id=3

--- Dev/src/com/TrueSculpt/TutorialWizard.java
+++ Dev/src/com/TrueSculpt/TutorialWizard.java
@@ -1,7 +1,40 @@
 package com.TrueSculpt;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class TutorialWizard extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.tutorialwizard);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+	}
 
 }
