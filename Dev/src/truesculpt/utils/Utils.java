31ff172d2079c3dc356b6c004d5980ad4888ffca
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-06 19:29:30 +0100

    dialogs ok

--- Dev/src/truesculpt/utils/Utils.java
+++ Dev/src/truesculpt/utils/Utils.java
@@ -1,5 +1,32 @@
 package truesculpt.utils;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.text.InputFilter.LengthFilter;
+import android.widget.Toast;
+
 public class Utils {
 
+	public static void StartMyActivity(Activity callingACtivity, String strClassName)
+	{
+		if (callingACtivity!=null)
+		{	
+			boolean bSuccess=true;
+			String msg="";
+			
+			Intent myIntent = new Intent();
+			myIntent.setClassName(callingACtivity, strClassName );			
+			try {
+				callingACtivity.startActivity(myIntent);
+			} catch (Exception e) {
+				msg=e.getMessage();		
+				bSuccess=false;
+			}
+			
+			if (!bSuccess)
+			{
+				Toast.makeText(callingACtivity, msg, Toast.LENGTH_LONG);
+			}
+		}
+	}
 }
