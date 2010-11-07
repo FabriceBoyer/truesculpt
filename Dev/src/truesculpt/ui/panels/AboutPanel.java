70f8baf4990fe0fe89f5cc8dfdf633fcfd93568e
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-07 14:31:34 +0100

    <

--- Dev/src/truesculpt/ui/panels/AboutPanel.java
+++ Dev/src/truesculpt/ui/panels/AboutPanel.java
@@ -1,7 +1,30 @@
 package truesculpt.ui.panels;
 
+import truesculpt.main.R;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class AboutPanel extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+		
+
+		final Button button = (Button) findViewById(R.id.Ok_btn);
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();				
+			}
+		});
+	}
 
 }
