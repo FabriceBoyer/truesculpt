8f6d6ca48a0669886f3d8cdda4fa20fd07f277bf
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-08 00:47:40 +0100

    math func

--- Dev/src/truesculpt/managers/PointOfViewManager.java
+++ Dev/src/truesculpt/managers/PointOfViewManager.java
@@ -4,9 +4,29 @@ import android.app.Activity;
 
 public class PointOfViewManager extends BaseManager {
 
+	//camera pos
+	private float mX=0.0f;
+	private float mY=0.0f;
+	private float mZ=10.0f;
+	
+	//looked at point
+	private float mXOrig=0.0f;
+	private float mYOrig=0.0f;
+	private float mZOrig=0.0f;
+	
 	public PointOfViewManager(Activity mBaseActivity) {
 		super(mBaseActivity);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void addZoomDistance(float dist)
+	{
+		
+	}
+	
+	public void addRotationAngle(float angle)
+	{
+		
 	}
 
 }
--- Dev/src/truesculpt/utils/Utils.java
+++ Dev/src/truesculpt/utils/Utils.java
@@ -1,5 +1,7 @@
 package truesculpt.utils;
 
+import java.util.Vector;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
@@ -7,6 +9,42 @@ import android.widget.Toast;
 
 public class Utils {
 
+	public static float DegToRad(float deg) {
+		return (float) (deg * Math.PI / 180.0f);
+	}
+	
+	//in degrees, return x,y,z in vector
+	public static Vector<Float> PolToCart(float theta, float phi, float psi, float R)
+	{
+		Vector<Float> res= new Vector<Float>(3);
+	
+		float x = (float) (R * Math.cos(DegToRad(theta)) * Math.cos(DegToRad(phi)));
+		float y = (float) (R * Math.sin(DegToRad(theta)) * Math.cos(DegToRad(phi)));
+		float z = (float) (R * Math.sin(DegToRad(phi)));
+		
+		res.add(x);
+		res.add(y);
+		res.add(z);
+		
+		return res;
+	}
+	
+	public static float Distance(Vector<Float> pt1, Vector<Float> pt2)
+	{
+		float dRes=0.0f;
+		int n=pt1.size();
+		if (n==pt2.size())
+		{
+			for (int i = 0; i < n; i++) {
+				float val1=pt1.get(i);
+				float val2=pt2.get(i);
+				
+				dRes+=Math.pow((val2-val1),2);
+			}			
+		}
+		return dRes;		
+	}
+	
 	public static void StartMyActivity(Activity callingActivity,
 			Class<?> cls) {
 		if (callingActivity != null) {
