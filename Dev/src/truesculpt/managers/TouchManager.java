92038c57d138a128d8e42124cd2fd05d7d305831
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-08 00:10:07 +0100

    <

--- Dev/src/truesculpt/managers/TouchManager.java
+++ Dev/src/truesculpt/managers/TouchManager.java
@@ -1,7 +1,10 @@
 package truesculpt.managers;
 
+import truesculpt.utils.Global;
 import android.app.Activity;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.*;
 
 //To detect sculpture action, zoom and pan actions based on gesture
 public class TouchManager extends BaseManager {
@@ -11,8 +14,13 @@ public class TouchManager extends BaseManager {
 		// TODO Auto-generated constructor stub
 	}
 
+	//ScaleGestureDetector mScaleGestureDetector = new ScaleGestureDetector();
 	public void onTouchEvent(MotionEvent event)
 	{
+		//String msg="Pressure = " + Float.toString(event.getPressure());
+		String msg="(x,y) = (" + Float.toString(event.getX()) +"," +Float.toString(event.getY()) + ")";
+
+		Log.i(Global.TAG,msg);
 		
 	}
 }
