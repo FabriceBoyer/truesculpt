37f603a7dbc8054242d4dcb276b2fa0d57704f1a
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-08 23:11:42 +0100

    <

--- Dev/src/truesculpt/ui/panels/HistoryPanel.java
+++ Dev/src/truesculpt/ui/panels/HistoryPanel.java
@@ -1,9 +1,19 @@
 package truesculpt.ui.panels;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 
 //For history of action manager
 public class HistoryPanel extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 
 }
