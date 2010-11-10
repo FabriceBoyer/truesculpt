cd16a183afc59deceff690c59ec24ed7197c4938
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-10 03:11:53 +0100

    <

--- Dev/src/truesculpt/ui/panels/PointOfViewPanel.java
+++ Dev/src/truesculpt/ui/panels/PointOfViewPanel.java
@@ -1,16 +1,20 @@
 package truesculpt.ui.panels;
 
+import truesculpt.main.Managers;
 import truesculpt.main.R;
+import truesculpt.main.TrueSculptApp;
+import truesculpt.managers.PointOfViewManager.OnPointOfViewChangeListener;
 import android.app.Activity;
 import android.os.Bundle;
 
 //TODO transparent to view result immediatly on lower activity
-public class PointOfViewPanel extends Activity {
+public class PointOfViewPanel extends Activity implements OnPointOfViewChangeListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.pointofview);
+		getManagers().getmPointOfViewManager().registerPointOfViewChangeListener(this);
 	}
 
 	@Override
@@ -26,5 +30,15 @@ public class PointOfViewPanel extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
+	}
+
+	@Override
+	public void onPointOfViewChange() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public Managers getManagers() {	
+		return ((TrueSculptApp)getApplicationContext()).getManagers();
 	}
 }
