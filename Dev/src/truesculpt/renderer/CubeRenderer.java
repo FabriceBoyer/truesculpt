58b05d1049287020663cbd8345eb50c6f1cf889c
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-11 18:27:49 +0100

    <

--- Dev/src/truesculpt/renderer/CubeRenderer.java
+++ Dev/src/truesculpt/renderer/CubeRenderer.java
@@ -16,7 +16,6 @@
 
 package truesculpt.renderer;
 
-import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
@@ -32,7 +31,8 @@ public class CubeRenderer implements GLSurfaceView.Renderer {
         mCube = new Cube();
     }
 
-    public void onDrawFrame(GL10 gl) {
+    @Override
+	public void onDrawFrame(GL10 gl) {
         /*
          * Usually, the first thing one might want to do is to clear
          * the screen. The most efficient way of doing this is to use
@@ -64,7 +64,8 @@ public class CubeRenderer implements GLSurfaceView.Renderer {
         mAngle += 1.2f;
     }
 
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
+    @Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
          gl.glViewport(0, 0, width, height);
 
          /*
@@ -79,7 +80,8 @@ public class CubeRenderer implements GLSurfaceView.Renderer {
          gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
     }
 
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+    @Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
         /*
          * By default, OpenGL enables features that improve quality
          * but reduce performance. One might want to tweak that
