781c6003a37aada515e7280cc35630d6b2eb794e
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-15 00:25:35 +0100

    <

--- Dev/src/truesculpt/renderer/CubeRenderer.java
+++ Dev/src/truesculpt/renderer/CubeRenderer.java
@@ -19,9 +19,6 @@ package truesculpt.renderer;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
-import truesculpt.main.Managers;
-import truesculpt.main.TrueSculptApp;
-
 import android.opengl.GLSurfaceView;
 
 /**
@@ -29,91 +26,89 @@ import android.opengl.GLSurfaceView;
  */
 
 public class CubeRenderer implements GLSurfaceView.Renderer {
-    private float mRot;
-    private float mDistance;
-    private float mElevation;
+	private float mRot;
+	private float mDistance;
+	private float mElevation;
 
-    private Cube mCube;
+	private Cube mCube;
 
-    private boolean mTranslucentBackground;
+	private boolean mTranslucentBackground;
 
-    public CubeRenderer(boolean useTranslucentBackground) {
-        mTranslucentBackground = useTranslucentBackground;
-        mCube = new Cube();
-    }
-    
-    public void onPointOfViewChange(float fRot, float fDistance, float fElevation)
-    {
-    	mRot=fRot;
-    	mDistance=fDistance;
-    	mElevation=fElevation;
-    }
- 
-    
-    @Override
+	public CubeRenderer(boolean useTranslucentBackground) {
+		mTranslucentBackground = useTranslucentBackground;
+		mCube = new Cube();
+	}
+
+	public void onPointOfViewChange(float fRot, float fDistance, float fElevation) {
+		mRot = fRot;
+		mDistance = fDistance;
+		mElevation = fElevation;
+	}
+
+	@Override
 	public void onDrawFrame(GL10 gl) {
-        /*
-         * Usually, the first thing one might want to do is to clear
-         * the screen. The most efficient way of doing this is to use
-         * glClear().
-         */
+		/*
+		 * Usually, the first thing one might want to do is to clear the screen.
+		 * The most efficient way of doing this is to use glClear().
+		 */
 
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 
-        /*
-         * Now we're ready to draw some 3D objects
-         */
+		/*
+		 * Now we're ready to draw some 3D objects
+		 */
 
-        gl.glMatrixMode(GL10.GL_MODELVIEW);
-        gl.glLoadIdentity();
-        gl.glTranslatef(0, 0,-mDistance);
-        gl.glRotatef(mRot,        0, 1, 0);
-        gl.glRotatef(mElevation,  1, 0, 0);
+		gl.glMatrixMode(GL10.GL_MODELVIEW);
+		gl.glLoadIdentity();
+		gl.glTranslatef(0, 0, -mDistance);
+		gl.glRotatef(mRot, 0, 1, 0);
+		gl.glRotatef(mElevation, 1, 0, 0);
 
-        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
 
-        mCube.draw(gl);
+		mCube.draw(gl);
 
-    }
-    @Override
+	}
+
+	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
-         gl.glViewport(0, 0, width, height);
+		gl.glViewport(0, 0, width, height);
 
-         /*
-          * Set our projection matrix. This doesn't have to be done
-          * each time we draw, but usually a new projection needs to
-          * be set when the viewport is resized.
-          */
+		/*
+		 * Set our projection matrix. This doesn't have to be done each time we
+		 * draw, but usually a new projection needs to be set when the viewport
+		 * is resized.
+		 */
 
-         float ratio = (float) width / height;
-         gl.glMatrixMode(GL10.GL_PROJECTION);
-         gl.glLoadIdentity();
-         gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
-    }
-    @Override
+		float ratio = (float) width / height;
+		gl.glMatrixMode(GL10.GL_PROJECTION);
+		gl.glLoadIdentity();
+		gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+	}
+
+	@Override
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        /*
-         * By default, OpenGL enables features that improve quality
-         * but reduce performance. One might want to tweak that
-         * especially on software renderer.
-         */
-        gl.glDisable(GL10.GL_DITHER);
+		/*
+		 * By default, OpenGL enables features that improve quality but reduce
+		 * performance. One might want to tweak that especially on software
+		 * renderer.
+		 */
+		gl.glDisable(GL10.GL_DITHER);
 
-        /*
-         * Some one-time OpenGL initialization can be made here
-         * probably based on features of this particular context
-         */
-         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
-                 GL10.GL_FASTEST);
+		/*
+		 * Some one-time OpenGL initialization can be made here probably based
+		 * on features of this particular context
+		 */
+		gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
 
-         if (mTranslucentBackground) {
-             gl.glClearColor(0,0,0,0);
-         } else {
-             gl.glClearColor(1,1,1,1);
-         }
-         gl.glEnable(GL10.GL_CULL_FACE);
-         gl.glShadeModel(GL10.GL_SMOOTH);
-         gl.glEnable(GL10.GL_DEPTH_TEST);
-    }
+		if (mTranslucentBackground) {
+			gl.glClearColor(0, 0, 0, 0);
+		} else {
+			gl.glClearColor(1, 1, 1, 1);
+		}
+		gl.glEnable(GL10.GL_CULL_FACE);
+		gl.glShadeModel(GL10.GL_SMOOTH);
+		gl.glEnable(GL10.GL_DEPTH_TEST);
+	}
 }
