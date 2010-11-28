e23d0541a5ae1e631316c5681b64da22f74624a0
fabrice boyer <fabrice.boyer@gmail.com>
2010-11-28 21:14:26 +0100

    <

---
+++ Dev/src/truesculpt/actions/SculptAction.java
@@ -0,0 +1,20 @@
+package truesculpt.actions;
+
+public class SculptAction extends BaseAction
+{
+
+	@Override
+	public boolean DoAction()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean UndoAction()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
