62dd09f5155245945565d8e5376546a40f0d4642
fabrice boyer <fabrice.boyer@gmail.com>
2011-01-01 20:08:05 +0100

    <

--- Dev/src/truesculpt/actions/BaseAction.java
+++ Dev/src/truesculpt/actions/BaseAction.java
@@ -3,12 +3,25 @@ package truesculpt.actions;
 public abstract class BaseAction
 {
 
-	public BaseAction()
+	public BaseAction(String strDescription)
 	{
-
+		this.mStrDescription = strDescription;
 	}
-
+	
+	String mStrDescription;
+	
+	public String getDescription()
+	{
+		return mStrDescription;
+	}
+	public void setDescription(String strDescription)
+	{
+		this.mStrDescription = strDescription;
+	}
+	
 	public abstract boolean DoAction();
 
 	public abstract boolean UndoAction();
+
+	
 }
--- Dev/src/truesculpt/actions/InitialCreationAction.java
+++ Dev/src/truesculpt/actions/InitialCreationAction.java
@@ -3,6 +3,12 @@ package truesculpt.actions;
 public class InitialCreationAction extends BaseAction
 {
 
+	public InitialCreationAction(String strDescription)
+	{
+		super(strDescription);
+		// TODO Auto-generated constructor stub
+	}
+
 	@Override
 	public boolean DoAction()
 	{
