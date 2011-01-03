ff47c4fe794547dcfd02a4a5f545b49db0144a98
fabrice boyer <fabrice.boyer@gmail.com>
2011-01-04 00:27:38 +0100

    Mesh startup

--- Dev/src/truesculpt/mesh/Edge.java
+++ Dev/src/truesculpt/mesh/Edge.java
@@ -4,11 +4,17 @@ import java.util.Vector;
 
 public class Edge
 {
+	public Edge(Vertex v0, Vertex v1)
+	{
+		V0=v0;
+		V1=v1;
+	}
+
 	public Vertex V0=null;
 	public Vertex V1=null;
 	
 	public Face F0=null;
 	public Face F1=null;
 	
-	public Vector<Edge> mEdgeList;
+	public Vector<Edge> mEdgeList= new Vector<Edge>();
 }
