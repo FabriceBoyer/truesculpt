2d397ef9ccb9fb733c9ebebf9ea4cc0f29c368b5
fabrice boyer <fabrice.boyer@gmail.com>
2011-01-04 23:40:39 +0100

    <

--- Dev/src/truesculpt/mesh/RenderFaceGroup.java
+++ Dev/src/truesculpt/mesh/RenderFaceGroup.java
@@ -1,6 +1,60 @@
 package truesculpt.mesh;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
+import javax.microedition.khronos.opengles.GL10;
+
 public class RenderFaceGroup
 {
+	private FloatBuffer mColorBuffer;
+	private ShortBuffer mIndexBuffer;
+	private FloatBuffer mVertexBuffer;
 
+	private Mesh mMesh=null;
+	public RenderFaceGroup(Mesh mesh)
+	{
+		mMesh=mesh;
+		
+		float one = 1.0f;
+		float vertices[] = { -one, -one, -one, one, -one, -one, one, one, -one, -one, one, -one, -one, -one, one, one, -one, one, one, one, one, -one, one, one, };
+		float colors[] = { 0, 0, 0, one, one, 0, 0, one, one, one, 0, one, 0, one, 0, one, 0, 0, one, one, one, 0, one, one, one, one, one, one, 0, one, one, one, };
+		short indices[] = { 0, 4, 5, 0, 5, 1, 1, 5, 6, 1, 6, 2, 2, 6, 7, 2, 7, 3, 3, 7, 4, 3, 4, 0, 4, 7, 6, 4, 6, 5, 3, 0, 1, 3, 1, 2 };
+
+		// Buffers to be passed to gl*Pointer() functions
+		// must be direct, i.e., they must be placed on the
+		// native heap where the garbage collector cannot
+		// move them.
+		//
+		// Buffers with multi-byte datatypes (e.g., short, int, float)
+		// must have their byte order set to native order
+
+		ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
+		vbb.order(ByteOrder.nativeOrder());
+		mVertexBuffer = vbb.asFloatBuffer();
+		mVertexBuffer.put(vertices);
+		mVertexBuffer.position(0);
+
+		ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+		cbb.order(ByteOrder.nativeOrder());
+		mColorBuffer = cbb.asFloatBuffer();
+		mColorBuffer.put(colors);
+		mColorBuffer.position(0);
+
+		ByteBuffer ibb = ByteBuffer.allocateDirect(indices.length * 2);
+		ibb.order(ByteOrder.nativeOrder());
+		mIndexBuffer = ibb.asShortBuffer();
+		mIndexBuffer.put(indices);
+		mIndexBuffer.position(0);
+	}
+
+	public void draw(GL10 gl)
+	{
+		gl.glFrontFace(GL10.GL_CCW);
+		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
+		gl.glColorPointer(4, GL10.GL_FLOAT, 0, mColorBuffer);
+		gl.glDrawElements(GL10.GL_TRIANGLES, 36, GL10.GL_UNSIGNED_SHORT, mIndexBuffer);
+	}
 }
