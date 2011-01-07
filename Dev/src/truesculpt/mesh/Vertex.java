3ca70e8c2ac7d92f9da2516ababa69e5af283f29
fabrice boyer <fabrice.boyer@gmail.com>
2011-01-07 01:50:25 +0100

    AutoSave

--- Dev/src/truesculpt/mesh/Vertex.java
+++ Dev/src/truesculpt/mesh/Vertex.java
@@ -2,8 +2,24 @@ package truesculpt.mesh;
 
 import java.util.Vector;
 
+import truesculpt.utils.MatrixUtils;
+
 public class Vertex
 {
+	
+	public Vertex(float [] coord)
+	{		
+		MatrixUtils.copy(coord, Coord);		
+	}
+	
+	public Vertex(Vertex V0, Vertex V1)
+	{
+		float[] temp= new float[3];
+		MatrixUtils.plus(V0.Coord, V1.Coord, temp);
+		MatrixUtils.scalarMultiply(temp, 0.5f);
+		MatrixUtils.copy(temp, Coord);
+	}
+	
 	public Vertex(float x, float y, float z)
 	{		
 		Coord[0]=x;
