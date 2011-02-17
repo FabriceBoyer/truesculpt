14c3eb642b8842e97e97016d2dc99a93f0725772
fabrice boyer <fabrice.boyer@gmail.com>
2011-02-17 22:11:00 +0100

    <

--- Dev/src/truesculpt/ui/panels/RendererMainPanel.java
+++ Dev/src/truesculpt/ui/panels/RendererMainPanel.java
@@ -292,17 +292,23 @@ public class RendererMainPanel extends Activity implements Observer
 		// Handle the back button
 		if (keyCode == KeyEvent.KEYCODE_BACK)
 		{
-			// Ask the user if they want to quit
-			new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle(R.string.quit).setMessage(R.string.really_quit).setPositiveButton(R.string.yes, new DialogInterface.OnClickListener()
+			if (mToolsSlidingDrawer.isOpened())
 			{
-				@Override
-				public void onClick(DialogInterface dialog, int which)
+				mToolsSlidingDrawer.close();
+			}
+			else
+			{
+				// Ask the user if they want to quit
+				new AlertDialog.Builder(this).setIcon(android.R.drawable.ic_dialog_alert).setTitle(R.string.quit).setMessage(R.string.really_quit).setPositiveButton(R.string.yes, new DialogInterface.OnClickListener()
 				{
-					// Stop the activity
-					finish();
-				}
-			}).setNegativeButton(R.string.no, null).show();
-
+					@Override
+					public void onClick(DialogInterface dialog, int which)
+					{
+						// Stop the activity
+						finish();
+					}
+				}).setNegativeButton(R.string.no, null).show();
+			}
 			return true;
 		} else
 		{
