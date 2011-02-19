0ac2a54510df5c13dbe37cfa4537dbc7d3c00781
fabrice boyer <fabrice.boyer@gmail.com>
2011-02-19 14:05:53 +0100

    <

--- Dev/src/truesculpt/ui/panels/FileSelectorPanel.java
+++ Dev/src/truesculpt/ui/panels/FileSelectorPanel.java
@@ -1,9 +1,16 @@
 package truesculpt.ui.panels;
 
+import truesculpt.main.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 //to open and close, link to website, MRU, 
 public class FileSelectorPanel extends Activity
 {
-
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fileselector);
+	}
 }
