2ed3350d3096369fdc05e01104ce111c84f12671
fabrice boyer <fabrice.boyer@gmail.com>
2011-02-22 23:01:05 +0100

    better sculpt

--- Dev/src/truesculpt/actions/SculptAction.java
+++ Dev/src/truesculpt/actions/SculptAction.java
@@ -1,21 +1,52 @@
 package truesculpt.actions;
 
+import java.util.ArrayList;
+
 import truesculpt.main.R;
+import truesculpt.mesh.Mesh;
+import truesculpt.mesh.Vertex;
+import truesculpt.utils.MatrixUtils;
+
 
 public class SculptAction extends BaseAction
 {
-
-	public SculptAction(String strDescription)
+	private class VertexChange
+	{
+		public VertexChange(int nIndex, float[] offset)
+		{
+			super();
+			this.nIndex = nIndex;
+			MatrixUtils.copy(offset, Voffset);
+		}
+		public int nIndex=-1;
+		float[] Voffset=new float[3];
+	}
+	
+	private ArrayList<VertexChange> mVertexChanges=new ArrayList<VertexChange>() ;
+	
+	public SculptAction()
 	{
 		super();
-		setDescription(strDescription);
+		setDescription("Sculpting");
 	}
 
 	@Override
 	public boolean DoAction()
 	{
-		// TODO Auto-generated method stub
-		return false;
+		//update normals and publish value after all updates
+		Mesh mesh=getManagers().getMeshManager().getMesh();
+		for (VertexChange change : mVertexChanges)
+		{
+			Vertex vertex=mesh.getVertexList().get(change.nIndex);
+			MatrixUtils.plus(vertex.Coord, change.Voffset, vertex.Coord);	
+		}
+		for (VertexChange change : mVertexChanges)
+		{
+			Vertex vertex=mesh.getVertexList().get(change.nIndex);
+			mesh.ComputeVertexNormal(vertex);
+			mesh.UpdateVertexValue(change.nIndex);
+		}
+		return true;
 	}
 
 	@Override
@@ -33,8 +64,24 @@ public class SculptAction extends BaseAction
 	@Override
 	public boolean UndoAction()
 	{
-		// TODO Auto-generated method stub
-		return false;
+		//update normals and publish value after all updates
+		Mesh mesh=getManagers().getMeshManager().getMesh();
+		for (VertexChange change : mVertexChanges)
+		{
+			Vertex vertex=mesh.getVertexList().get(change.nIndex);
+			MatrixUtils.minus(vertex.Coord, change.Voffset, vertex.Coord);	
+		}
+		for (VertexChange change : mVertexChanges)
+		{
+			Vertex vertex=mesh.getVertexList().get(change.nIndex);
+			mesh.ComputeVertexNormal(vertex);
+			mesh.UpdateVertexValue(change.nIndex);
+		}
+		return true;
 	}
 
+	public void AddVertexOffset(Integer i, float[] vOffset)
+	{
+		mVertexChanges.add(new VertexChange(i,vOffset));		
+	}
 }
