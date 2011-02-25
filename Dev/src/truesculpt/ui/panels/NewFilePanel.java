4c678a70d74e98ca437c8e21a380c0d8040e2af9
fabrice boyer <fabrice.boyer@gmail.com>
2011-02-25 01:07:12 +0100

    <

--- Dev/src/truesculpt/ui/panels/NewFilePanel.java
+++ Dev/src/truesculpt/ui/panels/NewFilePanel.java
@@ -6,11 +6,14 @@ import truesculpt.main.TrueSculptApp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class NewFilePanel extends Activity
 {
 	private Button mNewBtn;
+	private Spinner mSubdivion_level_spinner;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -25,15 +28,21 @@ public class NewFilePanel extends Activity
 			public void onClick(View v)
 			{
 				//TODO dialog for color and subdivion level and alert if new failed (already in init)
-				getManagers().getMeshManager().NewMesh(5);	
-				
-				String name=getManagers().getMeshManager().getName();
+				getManagers().getMeshManager().NewMesh(mSubdivion_level_spinner.getSelectedItemPosition());	
 				
 				getManagers().getUsageStatisticsManager().TrackEvent("NewFile", "New", 1);
 				
 				finish();
 			}
 		});
+		
+	    mSubdivion_level_spinner = (Spinner) findViewById(R.id.Subdivion_level_spinner);
+	    ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	            this, R.array.subdivision_levels, android.R.layout.simple_spinner_item);
+	    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+	    mSubdivion_level_spinner.setAdapter(adapter);
+	    mSubdivion_level_spinner.setSelection(5);
+
 	}
 	
 	public Managers getManagers()
