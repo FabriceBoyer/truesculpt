a68747917c9edc7667ce77bdfd6966c2fcfb66ca
fabrice boyer <fabrice.boyer@gmail.com>
2011-02-25 01:07:12 +0100

    <

--- Dev/src/truesculpt/ui/panels/OpenFilePanel.java
+++ Dev/src/truesculpt/ui/panels/OpenFilePanel.java
@@ -10,17 +10,27 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class OpenFilePanel extends Activity implements Runnable
 {
 	private Button mOpenBtn;
 	private ProgressDialog waitDialog=null;
+	private GridView gridview=null;
+	
 	private final int DIALOG_WAIT = 1;
 	
 	@Override
@@ -38,6 +48,61 @@ public class OpenFilePanel extends Activity implements Runnable
 				OpenInternal();
 			}
 		});	
+		
+		gridview = (GridView) findViewById(R.id.openfilegridview);
+	    gridview.setAdapter(new ImageAdapter(this));
+
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            Toast.makeText(OpenFilePanel.this, "" + position, Toast.LENGTH_SHORT).show();
+	        }
+	    });
+
+	}
+	
+	public class ImageAdapter extends BaseAdapter {
+	    private Context mContext;
+
+	    public ImageAdapter(Context c) {
+	        mContext = c;
+	    }
+
+	    public int getCount() {
+	        return mThumbIds.length;
+	    }
+
+	    public Object getItem(int position) {
+	        return null;
+	    }
+
+	    public long getItemId(int position) {
+	        return 0;
+	    }
+
+	    // create a new ImageView for each item referenced by the Adapter
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        ImageView imageView;
+	        if (convertView == null) {  // if it's not recycled, initialize some attributes
+	            imageView = new ImageView(mContext);
+	            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+	            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+	            imageView.setPadding(8, 8, 8, 8);
+	        } else {
+	            imageView = (ImageView) convertView;
+	        }
+
+	        imageView.setImageResource(mThumbIds[position]);
+	        return imageView;
+	    }
+
+	    // references to our images
+	    private Integer[] mThumbIds = {
+	            R.drawable.brush, R.drawable.colorpicker,
+	            R.drawable.copy, R.drawable.no_symmetry,
+	            R.drawable.disc, R.drawable.smooth,
+	            R.drawable.equalizer, R.drawable.draw,
+	            R.drawable.clock, R.drawable.wizard
+	    };
 	}
 	
 	@Override
