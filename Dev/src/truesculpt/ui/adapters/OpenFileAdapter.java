5a1870331ae77be299b9a048f36fb9f62dd6877c
fabrice boyer <fabrice.boyer@gmail.com>
2011-02-28 21:58:43 +0100

    <

--- Dev/src/truesculpt/ui/adapters/OpenFileAdapter.java
+++ Dev/src/truesculpt/ui/adapters/OpenFileAdapter.java
@@ -2,6 +2,7 @@ package truesculpt.ui.adapters;
 
 import java.util.ArrayList;
 import truesculpt.main.R;
+import truesculpt.ui.adapters.HistoryAdapter.ViewHolder;
 import truesculpt.ui.panels.OpenFilePanel.FileElem;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -18,6 +19,13 @@ public class OpenFileAdapter extends BaseAdapter
 {
     private ArrayList<FileElem> mFileList=null;
     
+	class ViewHolder
+	{
+		TextView title;
+		ImageView image;
+		Bitmap bmp;
+	}
+	
     public OpenFileAdapter(Context context, ArrayList<FileElem> fileList) 
     {
     	mFileList = fileList;
@@ -44,26 +52,33 @@ public class OpenFileAdapter extends BaseAdapter
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent)
     {
+		ViewHolder holder;
+		
+		FileElem elem=mFileList.get(position);
+		
 		if (convertView == null)
 		{	
+			holder = new ViewHolder();
+			
 			convertView = inflater.inflate(R.layout.openfileitem, null);
 			convertView.setLayoutParams(new GridView.LayoutParams(200,200));
 			convertView.setPadding(8, 8, 8, 8);
-			TextView title = (TextView) convertView.findViewById(R.id.fileopentitle);
-
-			ImageView image = (ImageView) convertView.findViewById(R.id.fileopenimage);
-			image.setScaleType(ImageView.ScaleType.FIT_CENTER);	
-
-			FileElem elem=mFileList.get(position);
-			title.setText(elem.name);
-			
-			Bitmap bmp = BitmapFactory.decodeFile(elem.imagefilename);
-			image.setImageBitmap(bmp);
-			
-			title.setEnabled(true);
-			title.setVisibility(View.VISIBLE);
+			holder.title = (TextView) convertView.findViewById(R.id.fileopentitle);
+			holder.image = (ImageView) convertView.findViewById(R.id.fileopenimage);
+			holder.image.setScaleType(ImageView.ScaleType.FIT_CENTER);	
+			holder.bmp = BitmapFactory.decodeFile(elem.imagefilename);
+			convertView.setTag(holder);
 		} 
-        
+		else
+		{
+			holder = (ViewHolder) convertView.getTag();
+		}         
+
+		holder.title.setText(elem.name);			
+		holder.image.setImageBitmap(holder.bmp);			
+		holder.title.setEnabled(true);
+		holder.title.setVisibility(View.VISIBLE);
+		
         return convertView;
     } 
 }
