9a8b55277dc9665bc30ebc8994ef49c67e33e4a0
fabrice boyer <fabrice.boyer@gmail.com>
2011-03-09 21:09:18 +0100

    <

--- Dev/src/truesculpt/ui/adapters/OpenFileAdapter.java
+++ Dev/src/truesculpt/ui/adapters/OpenFileAdapter.java
@@ -3,21 +3,30 @@ package truesculpt.ui.adapters;
 import java.util.ArrayList;
 import truesculpt.main.R;
 import truesculpt.ui.adapters.HistoryAdapter.ViewHolder;
+import truesculpt.ui.adapters.ImageLoader.ImageLoadListener;
 import truesculpt.ui.panels.OpenFilePanel.FileElem;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.ViewSwitcher;
 
-public class OpenFileAdapter extends BaseAdapter 
+public class OpenFileAdapter extends BaseAdapter implements ImageLoadListener
 {
     private ArrayList<FileElem> mFileList=null;
+	private Handler mHandler=null;
+	private ImageLoader mImageLoader = null;
+    private LayoutInflater inflater=null;
+    private Context mContext=null;
+    
     
 	class ViewHolder
 	{
@@ -27,8 +36,25 @@ public class OpenFileAdapter extends BaseAdapter
 	
     public OpenFileAdapter(Context context, ArrayList<FileElem> fileList) 
     {
+    	mImageLoader = new ImageLoader(this);
+    	mImageLoader.setPriority(Thread.MIN_PRIORITY);
+    	mImageLoader.start();
+    	while (!mImageLoader.IsReady())
+    	{
+    		try
+			{
+				Thread.sleep(100);
+			} 
+    		catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+    	}
+    	mHandler = new Handler();
+    	
     	mFileList = fileList;
     	inflater = LayoutInflater.from(context);
+    	mContext=context;
     }
 
     public int getCount()
@@ -46,12 +72,10 @@ public class OpenFileAdapter extends BaseAdapter
         return position;
     }
 
-    LayoutInflater inflater;
-    
     // create a new ImageView for each item referenced by the Adapter
     public View getView(int position, View convertView, ViewGroup parent)
-    {
-		ViewHolder holder;
+    {		
+    	ViewHolder holder;
 		
 		FileElem elem=mFileList.get(position);
 		
@@ -73,33 +97,40 @@ public class OpenFileAdapter extends BaseAdapter
 		holder.title.setText(elem.name);	
 		if (elem.bmp==null)
 		{
-			try
-			{
-				Bitmap src=BitmapFactory.decodeFile(elem.imagefilename);
-				float w=src.getWidth();
-				float h=src.getHeight();
-				float ratio=w/h;
-				if (ratio>100/200)
-				{
-					w=100;
-					h= (w/ratio);
-				}
-				else
-				{
-					h=200;
-					w=(h*ratio);					
-				}
-				elem.bmp = Bitmap.createScaledBitmap(src,(int)w,(int)h,false);
-			}
-			catch(Exception e)
-			{
-				elem.bmp=null;
-			}
+			mImageLoader.queueImageLoad(holder, elem);
 		}
 		holder.image.setImageBitmap(elem.bmp);
 		holder.title.setEnabled(true);
 		holder.title.setVisibility(View.VISIBLE);
 		
         return convertView;
-    } 
+    }
+    
+    public void handleImageLoaded( 	
+    		final ViewHolder holder,
+    		final FileElem elem,
+    		final Bitmap aBitmap) 
+    {
+		// The enqueue the following in the UI thread
+		mHandler.post(new Runnable()
+		{
+    		public void run() 
+    		{
+		    	// set the bitmap in the ImageView
+    			holder.image.setImageBitmap(aBitmap);
+    			elem.bmp=aBitmap;
+    		}
+		});
+	}
+    
+    @Override
+    protected void finalize() throws Throwable 
+    {
+	    // TODO Auto-generated method stub
+	    super.finalize();
+	
+	    // stop the thread we started
+	    mImageLoader.stopThread();
+    }
+
 }
