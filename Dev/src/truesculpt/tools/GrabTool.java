b0c104115c4ceeb78d86f194957cb25fefb3086a
fabrice boyer <fabrice.boyer@gmail.com>
2011-11-03 00:35:24 +0100

    tools lib

---
+++ Dev/src/truesculpt/tools/GrabTool.java
@@ -0,0 +1,14 @@
+package truesculpt.tools;
+
+import truesculpt.main.Managers;
+
+public class GrabTool extends SculptingTool
+{
+
+	public GrabTool(Managers managers)
+	{
+		super(managers);
+
+	}
+
+}
